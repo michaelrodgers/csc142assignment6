0e6c53e8748b49a833d7293b55e57739fac6f3a6
Lindsay Gilbert <Lindsaygilbert722@gmail.com>
2018-11-12 19:57:47 -0800

    created hashmap, iterating through and printing keys and values

--- Main.java
+++ Main.java
@@ -1,14 +1,19 @@
-import java.io.*;
-import java.util.*;
+package project2;
 
-public class Main {
-	
-	private static String file = "../../Documents/7_04_18.csv";
-	
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.Scanner;
+
+public class csc142assignment6 {
+
+	private static String file = "/Users/lindsaygilbert/Desktop/Projects/java-class/CSC142Project2/7_04_18.csv";
+
 	public static void main(String[] args) throws FileNotFoundException {
-		
+
 		try {
-		
+
 			Scanner sc = new Scanner(new File(file));
 			String[] geigerData = new String[64];
 			String dateTime = new String();
@@ -23,12 +28,16 @@ public class Main {
 			double dailyAverage = 0.0;
 			double highestDailyAverage = 0.0;
 			String campingTrip = new String();
-			
+
 			geigerData = sc.nextLine().split(",");
+			LinkedHashMap<String, Integer> map = new LinkedHashMap<String, Integer>();
+
 			while (sc.hasNextLine()) {
+				//date and time stamp
 				dateTime = geigerData[0];
+
 				try {
-					Integer.parseInt(geigerData[2]); 
+					Integer.parseInt(geigerData[2]);
 					isInt = true;
 				}
 				catch (Exception e) {
@@ -38,12 +47,12 @@ public class Main {
                 	cpm = Integer.parseInt(geigerData[2]);
                 	if (cpm >= high) {
                 		high = cpm;
-                	}            	
+                	}
 				}
                 if (cpm >= (high-5)) {
-                	System.out.println(geigerData[0] + "  " + geigerData[2]);
+                	map.put(geigerData[0], cpm);
                 }
-                
+
                 date = dateTime.split(" ")[0];
                 if (date.contentEquals(previousDate)) {
                 	sumTotal = sumTotal + cpm;
@@ -60,14 +69,20 @@ public class Main {
                 	previousDate = date;
                 }
 				geigerData = sc.nextLine().split(",");
-				while (geigerData.length <= 2) {geigerData = sc.nextLine().split(",");}	
+				while (geigerData.length <= 2) {geigerData = sc.nextLine().split(",");}
+			}
+			for (Map.Entry<String, Integer> entry : map.entrySet()) {
+				if (entry.getValue() >= (high - 5)) {
+					System.out.println(entry.getKey() + " " + entry.getValue());
+				}
 			}
 			System.out.println("We suspect the camping trip took place on: " + campingTrip);
 			sc.close();
 		}
-		
+
 		catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
+
 }
