423caa0231b878a92b6ba8793063d34a61c98ccb
Lindsay Gilbert <Lindsaygilbert722@gmail.com>
2018-11-17 18:25:18 -0800

    added histogram

--- Main.java
+++ Main.java
@@ -3,7 +3,9 @@ package project2;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class csc142assignment6 {
@@ -20,17 +22,20 @@ public class csc142assignment6 {
 			int cpm = 0;
 			Boolean isInt = false;
 			int high = 0;
+			int low = (int) Double.POSITIVE_INFINITY;
 			String date = new String();
 			String previousDate = new String();
 			previousDate = "4/6/2018";
 			double sumTotal = 0.0;
 			int count = 0;
+			int dataPoints = 0;
 			double dailyAverage = 0.0;
 			double highestDailyAverage = 0.0;
 			String campingTrip = new String();
 
 			geigerData = sc.nextLine().split(",");
 			LinkedHashMap<String, Integer> map = new LinkedHashMap<String, Integer>();
+			LinkedHashMap<Integer, Integer> histogramMap = new LinkedHashMap<Integer, Integer>();
 
 			while (sc.hasNextLine()) {
 				//date and time stamp
@@ -44,9 +49,15 @@ public class csc142assignment6 {
 					isInt = false;
 				}
                 if (isInt) {
+                	dataPoints++;
                 	cpm = Integer.parseInt(geigerData[2]);
+                	int cpmCount = histogramMap.containsKey(cpm) ? histogramMap.get(cpm) : 0;
+                	histogramMap.put(cpm, cpmCount + 1);
                 	if (cpm >= high) {
                 		high = cpm;
+                	}
+                	if (cpm < low) {
+                		low = cpm;
                 	}
 				}
                 if (cpm >= (high-5)) {
@@ -75,6 +86,28 @@ public class csc142assignment6 {
 				if (entry.getValue() >= (high - 5)) {
 					System.out.println(entry.getKey() + " " + entry.getValue());
 				}
+			}
+
+			int bin = 4;
+			int scale = 1000;
+			int binLow = 1;
+			System.out.println("histogram of values, each * represents 1000");
+			while (binLow + bin <= 40) {
+				int frequency = 0;
+				System.out.print(binLow + "-" + (binLow + bin) + " ");
+				for (int i = binLow; i <= binLow + bin; i++) {
+					for (Map.Entry<Integer, Integer> histogramEntry : histogramMap.entrySet()) {
+						if (histogramEntry.getKey() == i) {
+							frequency += Math.ceil(histogramEntry.getValue() / scale);
+						}
+					}
+
+				}
+				for (int j = 0; j <= frequency; j++) {
+					System.out.print("*");
+				}
+				binLow = binLow + bin + 1;
+				System.out.println("");
 			}
 			System.out.println("We suspect the camping trip took place on: " + campingTrip);
 			sc.close();
